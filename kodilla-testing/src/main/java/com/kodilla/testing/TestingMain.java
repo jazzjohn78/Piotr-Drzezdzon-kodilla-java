e653e63bb68f20167bd1095d5e6cacd2c55c2a4b
Piotr Drzezdzon <jazzjohn78@gmail.com>
2023-02-02 18:57:55 +0100

    6.2 Added classes SimpleUser and Calculator with tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        //test klasy SimpleUser
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if(result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //test klasy Calculator
+        Calculator calculator = new Calculator();
+        double addResult = calculator.add(2.5, 49.1);
+        if(addResult == 51.6){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        double subtractResult = calculator.subtract(142.12, 34.53);
+        if(subtractResult == 107.59){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
